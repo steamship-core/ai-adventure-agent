b210f6336eca6374be8679be96c933494eef7c13
Ted Benson <ted@steamship.com>
2023-10-11 14:47:08 -0400

    Remove logan

--- src/schema/camp.py
+++ src/schema/camp.py
@@ -20,13 +20,6 @@ class Camp(BaseModel):
                     Item(name="A mysterious orb. It glows with a strange energy.")
                 ],
             ),
-            NpcCharacter(
-                name="Logan",
-                description="A woodsman who was passing by and thought he'd join your crew.",
-                category="conversational",
-                background="From a small town. Grew up the youngest of three brothers. Mostly kept to himself until he a tornado swept away his whole family. Has been on his own since age 13. Knows a lot about the world. Old heart. Wise eyes. Tends to speak softly but with good advice.",
-                motivation="Just passing through, but thought he'd stay for a bit.",
-            ),
         ],
         description="The list of NPCs who are at the camp.",
     )
