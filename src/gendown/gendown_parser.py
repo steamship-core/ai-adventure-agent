ca9cd516a2f2e1cdaa0a63e9b88e192669fdc45e
Ted Benson <ted@steamship.com>
2023-10-21 10:57:27 -0700

    d

--- src/gendown/gendown_parser.py
+++ src/gendown/gendown_parser.py
@@ -9,6 +9,15 @@ from pydantic import BaseModel, Field
 from steamship.agents.schema import AgentContext
 
 
+def safe_format(text: str, params: dict) -> str:
+    """Safely formats a user-provided string by replacing {key} with `value` for all (key,value) pairs in `params`."""
+    ret = text
+    for (key, value) in params.items():
+        ret = ret.replace("{" + key + "}", value)
+
+    return ret
+
+
 class NodeKind(str, Enum):
     scope = "scope"
     output = "output"
