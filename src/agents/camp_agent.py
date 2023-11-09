145bc2f1625695ba56b7f211bb29b7c167ce624e
Dave Kolas <4643013+dkolas@users.noreply.github.com>
2023-11-09 11:56:43 -0500

    Update camp_agent.py

--- src/agents/camp_agent.py
+++ src/agents/camp_agent.py
@@ -1,21 +1,7 @@
-from typing import List
-
 from steamship import Block
 from steamship.agents.schema import Action, Agent, AgentContext, FinishAction
-from steamship.agents.schema.message_selectors import MessageWindowMessageSelector
 
 from tools.start_quest_tool import StartQuestTool
-from utils.moderation_utils import is_block_excluded
-
-
-class NonExcludedMessageWindowSelector(MessageWindowMessageSelector):
-    def get_messages(self, messages: List[Block]) -> List[Block]:
-        # choice here: if the user has been submitting a bunch of problematic conversation
-        #              then we are ok "forgetting" the distant context. so, we can trim from
-        #              the discovered window, rather than first filtering and then selecting
-        #              via the window.
-        context_messages = super().get_messages(messages)
-        return [msg for msg in context_messages if not is_block_excluded(msg)]
 
 
 class CampAgent(Agent):
