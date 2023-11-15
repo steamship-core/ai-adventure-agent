af006e735164788e4fb8fe8c082836bf280d2ae5
Douglas Reid <doug@steamship.com>
2023-11-15 10:22:28 -0800

    add onboarding endpoints for validation

--- src/endpoints/onboarding_endpoints.py
+++ src/endpoints/onboarding_endpoints.py
@@ -1,14 +1,17 @@
+from typing import Optional
+
 from steamship import Steamship, SteamshipError
 from steamship.agents.llms.openai import ChatOpenAI
 from steamship.agents.service.agent_service import AgentService
 from steamship.invocable import post
 from steamship.invocable.package_mixin import PackageMixin
 
-from agents.onboarding_agent import OnboardingAgent
+from agents.onboarding_agent import OnboardingAgent, _is_allowed_by_moderation
+from generators.generator_context_utils import get_image_generator
 from schema.game_state import ActiveMode
 
 # An instnace is a game instance.
-from utils.context_utils import RunNextAgentException, get_game_state
+from utils.context_utils import RunNextAgentException, get_game_state, save_game_state
 from utils.error_utils import record_and_throw_unrecoverable_error
 from utils.generation_utils import generate_story_intro
 
@@ -23,11 +26,83 @@ class OnboardingMixin(PackageMixin):
         self.client = client
         self.agent_service = agent_service
 
+    @post("/onboarding/character/name")
+    def set_character_name(self, name: str, context_id: Optional[str] = None, **kwargs):
+        context = self.agent_service.build_default_context(
+            context_id=context_id, **kwargs
+        )
+
+        if not _is_allowed_by_moderation(name, context):
+            raise SteamshipError(
+                "Supplied 'name' was rejected by game's moderation filter. Please try again."
+            )
+
+        game_state = get_game_state(context)
+        game_state.player.name = name
+        save_game_state(game_state, context)
+
+    @post("/onboarding/character/background")
+    def set_character_background(
+        self, background: str, context_id: Optional[str] = None, **kwargs
+    ):
+        context = self.agent_service.build_default_context(
+            context_id=context_id, **kwargs
+        )
+
+        if not _is_allowed_by_moderation(background, context):
+            raise SteamshipError(
+                "Supplied 'background' was rejected by game's moderation filter. Please try again."
+            )
+
+        game_state = get_game_state(context)
+        game_state.player.background = background
+        save_game_state(game_state, context)
+
+    @post("/onboarding/character/description")
+    def set_character_description(
+        self,
+        description: str,
+        update: Optional[bool] = True,
+        context_id: Optional[str] = None,
+        **kwargs,
+    ):
+        context = self.agent_service.build_default_context(
+            context_id=context_id, **kwargs
+        )
+
+        if not _is_allowed_by_moderation(description, context):
+            raise SteamshipError(
+                "Supplied 'description' was rejected by game's moderation filter. Please try again."
+            )
+
+        game_state = get_game_state(context)
+        game_state.player.description = description
+
+        if game_state.player.description and game_state.player.name:
+            if (not game_state.image_generation_requested()) or update:
+                if image_gen := get_image_generator(context):
+                    task = image_gen.request_profile_image_generation(context=context)
+                    character_image_block = task.wait().blocks[0]
+                    game_state.player.profile_image_url = (
+                        character_image_block.raw_data_url
+                    )
+                    game_state.profile_image_url = character_image_block.raw_data_url
+                    save_game_state(game_state, context)
+
+        save_game_state(game_state, context)
+
     @post("/complete_onboarding")
-    def complete_onboarding(self, **kwargs) -> bool:
+    def complete_onboarding(self, context_id: Optional[str] = None, **kwargs) -> bool:
         """Attempts to complete onboarding."""
         try:
-            context = self.agent_service.build_default_context()
+            context = self.agent_service.build_default_context(context_id=context_id)
+            game_state = get_game_state(context)
+
+            # TODO: streamline for mass validation ?
+            self.set_character_name(game_state.player.name)
+            self.set_character_background(game_state.player.background)
+            self.set_character_description(game_state.player.description)
+
             game_state = get_game_state(context)
 
             if game_state.active_mode != ActiveMode.ONBOARDING:
