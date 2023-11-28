410e28d3ccf29ce008419d95dd1d04111e66941d
Ted Benson <ted@steamship.com>
2023-11-28 13:07:20 -0500

    fix

--- tests/steamship_tests/schema/test_server_settings.py
+++ tests/steamship_tests/schema/test_server_settings.py
@@ -1,3 +1,4 @@
+from schema.image_theme import ImageTheme
 from schema.server_settings import ServerSettings
 
 
@@ -5,3 +6,11 @@ def test_server_settings_extra_field():
     ss = ServerSettings.parse_obj({"characters": []})
     assert ss
     assert not hasattr(ss, "characters")
+
+
+def test_server_settings_nested_obj():
+    ss = ServerSettings.parse_obj({"image_themes": [{"name": "test"}]})
+    assert ss
+    assert ss.image_themes
+    assert len(ss.image_themes) == 1
+    assert isinstance(ss.image_themes[0], ImageTheme)
