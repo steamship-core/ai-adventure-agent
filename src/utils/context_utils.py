62f3933e0b0183fe7ea979fd1bf4b71a2ce6733b
Dave Kolas <4643013+dkolas@users.noreply.github.com>
2023-11-30 13:49:43 -0500

    gpt 4 preview (#41)
    * gpt 4 preview
    
    * add back gpt-4 as possibility just in case

--- src/utils/context_utils.py
+++ src/utils/context_utils.py
@@ -72,7 +72,7 @@ def get_story_text_generator(
         game_state = get_game_state(context)
         preferences = game_state.preferences
 
-        open_ai_models = ["gpt-3.5-turbo", "gpt-4"]
+        open_ai_models = ["gpt-3.5-turbo", "gpt-4-1106-preview", "gpt-4"]
         replicate_models = ["dolly_v2", "llama_v2"]
 
         model_name = server_settings._select_model(
