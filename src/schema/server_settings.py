7730283e194dbd0ad779497d1aa055d9e96eb9e5
Ted Benson <edward.benson@gmail.com>
2023-12-07 15:57:03 -0500

    Update src/schema/server_settings.py
    Co-authored-by: Dave Kolas <4643013+dkolas@users.noreply.github.com>

--- src/schema/server_settings.py
+++ src/schema/server_settings.py
@@ -503,7 +503,7 @@ Can include descriptions of genre, characters, specific items and locations that
     narration_multilingual: Optional[bool] = SettingField(
         default=False,
         label="Narration includes non-English?",
-        description="Set to true if the story include non-English content?",
+        description="Set to true if the story include non-English content",
         type="boolean",
         required=False,
     )
